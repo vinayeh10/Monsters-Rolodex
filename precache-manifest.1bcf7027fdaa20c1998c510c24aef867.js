1cc589857e32de70805154c41c71be54d6e8dc6d
vinay.n <vinay.n@tavant.com>
2020-04-10 00:51:59 +0530

    Updates

---
+++ precache-manifest.1bcf7027fdaa20c1998c510c24aef867.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2475dd0d83762656f368f866f1d81636",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "d0ee02df54f84ad5948a",
+    "url": "/Monsters-Rolodex/static/css/main.f5b1a097.chunk.css"
+  },
+  {
+    "revision": "50178188a875b58681d5",
+    "url": "/Monsters-Rolodex/static/js/2.e9265ccf.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Monsters-Rolodex/static/js/2.e9265ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0ee02df54f84ad5948a",
+    "url": "/Monsters-Rolodex/static/js/main.79390483.chunk.js"
+  },
+  {
+    "revision": "9ab51a9735f2add4dd53",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.d8be1058.js"
+  }
+]);
